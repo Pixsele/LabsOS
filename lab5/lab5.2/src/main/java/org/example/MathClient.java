5d00eeeae9a0b8893277a55dba9b6de141ace688
Kirill Vorobyev <Kirill2005vorob200@yandex.ru>
2025-04-07 00:17:46 +0400

    add 5 lab

---
+++ lab5/lab5.2/src/main/java/org/example/MathClient.java
@@ -0,0 +1,41 @@
+package org.example;
+
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class MathClient{
+
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 12345;
+
+
+    public static void main(String[] args) {
+
+        try (Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Connected to server.");
+
+            while (true) {
+                System.out.print("Enter expression: ");
+                String input = scanner.nextLine();
+                out.println(input);
+                String response = in.readLine();
+
+                if (response == null) {
+                    System.out.println("Server disconnected.");
+                    break;
+                }
+
+                System.out.println("Server: " + response);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Connection failed: " + e.getMessage());
+        }
+    }
+}
